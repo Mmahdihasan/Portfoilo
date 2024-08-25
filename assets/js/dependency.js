a25a7ac65d6f1940ec76555c1bad8d2452fb71ac
Mmahdihasan <mahdistu01999@gmail.com>
2024-08-25 11:10:01 +0600

    counter

---
+++ assets/js/dependency.js
@@ -0,0 +1,26 @@
+var counter_list = [
+    {
+        counter_amount: 140,
+        counter_unit: "K",
+        counter_label_top: "Happy",
+        counter_label_bottom: "Clients"
+    },
+    {
+        counter_amount: 60,
+        counter_unit: "+",
+        counter_label_top: "Successful",
+        counter_label_bottom: "Projects"
+    },
+    {
+        counter_amount: 5,
+        counter_unit: "+",
+        counter_label_top: "Years",
+        counter_label_bottom: "Experince"
+    },
+    {
+        counter_amount: 15,
+        counter_unit: "+",
+        counter_label_top: "Partners",
+        counter_label_bottom: ""
+    }
+];
